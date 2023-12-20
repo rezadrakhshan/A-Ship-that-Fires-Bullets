c387105a086524eab97e6226d4ecc74994ebeb57
reza drakhshan <aghayebozorg1386@gmail.com>
2023-12-20 17:17:39 +0330

    12/20/2023

--- game_stats.py
+++ game_stats.py
@@ -2,7 +2,7 @@ class GameStats:
     def __init__(self, ai_game):
         self.settings = ai_game.setting
         self.reset_stats()
-        self.game_active = False
+        self.game_active = True
 
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
