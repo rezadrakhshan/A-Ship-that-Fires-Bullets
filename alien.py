ce839f329290bfbf237d5cdaecca3c69b43b1ed2
reza drakhshan <aghayebozorg1386@gmail.com>
2023-12-20 08:29:17 +0330

    Update Ship and Alien

--- alien.py
+++ alien.py
@@ -7,7 +7,7 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.setting
-        self.image = pygame.image.load("images/alien.jpeg")
+        self.image = pygame.image.load("images/alien.png")
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship:
     def __init__(self, ai_game) -> None:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load("images/ship.png")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.moving_right = False
